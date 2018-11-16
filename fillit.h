07dc989038cfc5240b4d707ee817f9468a4ec266
Yoann RIBEIRO <yoribeir@e3r9p13.42.fr>
2018-11-16 19:28:49 +0100

    yalouf 🍆

---
+++ fillit.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoribeir <yoribeir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/16 19:17:29 by yoribeir          #+#    #+#             */
+/*   Updated: 2018/11/16 19:27:52 by yoribeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include "libft.h"
+
+/*
+** By Yalaouf, made with 🤡🍆🍆🍆🍆🍆🍆🍆🍆🍆🍆🍆🍆
+*/
+
+typedef struct		s_tetri
+{
+	char			**shape;
+	struct s_tetri	*next;
+}					t_tetri;
+
+#endif
