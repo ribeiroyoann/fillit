d5ebc27caa28371db167db6b5793a22dee89b552
Oussama MAYSOUR <oumaysou@e3r10p12.42.fr>
2018-11-20 15:13:38 +0100

    correction de t_list

--- ft_list_push_back.c
+++ ft_list_push_back.c
@@ -6,15 +6,15 @@
 /*   By: oumaysou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 15:02:57 by oumaysou          #+#    #+#             */
-/*   Updated: 2018/11/20 15:08:17 by oumaysou         ###   ########.fr       */
+/*   Updated: 2018/11/20 15:13:01 by oumaysou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-void	ft_list_push_back(t_list **list, t_list *new)
+void	ft_list_push_back(t_tetri **list, t_tetri *new)
 {
-	t_list *tmp;
+	t_tetri *tmp;
 
 	tmp = *list;
 	while (tmp->next)
