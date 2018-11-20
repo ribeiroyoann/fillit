0c89aa209748e88f9dffe7daddcc4fa50d6860d3
Oussama MAYSOUR <oumaysou@e3r10p12.42.fr>
2018-11-20 14:47:24 +0100

    version 1 parser

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: yoribeir <yoribeir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 19:17:19 by yoribeir          #+#    #+#             */
-/*   Updated: 2018/11/20 13:53:34 by oumaysou         ###   ########.fr       */
+/*   Updated: 2018/11/20 14:46:24 by oumaysou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,27 @@
 
 int		parser(char **argv)
 {
-	int		fd;
-	char	*line;
-	char	**tab;
+	int				fd;
+	unsigned int	i;
+	char			*line;
+	t_tetri			*list;
 
 	fd = open(argv[1], O_RDONLY);
-	while (get_next_line(fd, &line) == 1)
+	i = 0;
+	while (get_next_line(fd, &line) == 1 && i < 4)
 	{
-		printf("[%s]", line);
+		if (!i)
+			if ((list->shape = (char **)malloc(sizeof(char *) * 5)) == NULL)
+				return (0);
+		if (!(i % 4))
+			(list->shape)[i] = ft_strdup(*line);
+		i++;
+		if (i == 4)
+		{	
+			list->shape[i] = 0;
+			list = list->next;
+			i = 0;
+		}
 	}
-
 	return (1);
 }
