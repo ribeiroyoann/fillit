6aebc8cc83e35a00c0b65dfcf20dac19a0397dbe
root <root@Yoann.localdomain>
2018-11-30 18:18:06 +0100

    reader ok

--- reader.c
+++ reader.c
@@ -6,13 +6,34 @@
 /*   By: anonymous <anonymous@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 15:46:34 by anonymous         #+#    #+#             */
-/*   Updated: 2018/11/28 20:49:21 by anonymous        ###   ########.fr       */
+/*   Updated: 2018/11/30 18:17:54 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
 #define TETRI_SIZE 21
+
+int		check_links(char *buffer)
+{
+	int		i;
+	int		count;
+
+	i = 0;
+	count = 0;
+	while (i < 20)
+	{
+		if (buffer[i] == '#')
+		{
+			if (buffer[i + 1] == '#')
+				count++;
+			if (buffer[i + 5] == '#')
+				count++;
+		}
+		i++;
+	}
+	return (count == 3 || count == 4);
+}
 
 int		check_buffer(char *buffer)
 {
@@ -27,14 +48,12 @@ int		check_buffer(char *buffer)
 	{
 		if (buffer[i] != '#' && buffer[i] != '.' && buffer[i] != '\n')
 		{
-			printf("[%c]\n", buffer[i]);
-			printf("ERROR\n");
+			printf("ERROR CHARS\n");
 			return (0);
 		}
 		if (buffer[offset] != '\n' && offset < 16)
 		{
-			printf("[%c]\n", buffer[offset]);
-			printf("ERROR1\n");
+			printf("ERROR NEWL\n");
 			return (0);
 		}
 		if (buffer[i] == '#')
@@ -81,25 +100,19 @@ int		reader(int fd)
 	while ((readsz = read(fd, buffer, TETRI_SIZE)))
 	{
 		buffer[readsz] = '\0';
-		if (check_buffer(buffer))
+		if (!check_buffer(buffer))
 		{
-			tetri = get_tetris(buffer);
-			print_tetri(tetri);
-			if (!check_shape(tetri))
-			{
-				printf("SHAPE ERROR\n");
-				return (0);
-			}
-			printf("SHAPE OK\n");
-			ft_lstpushback(&list, ft_lstnew(tetri, 32));
-		}
-		else
-		{
-			printf("ERROR2\n");
-			free_list(list);
+			printf("ERROR BUFFER\n");
 			return (0);
 		}
+		if (!check_links(buffer))
+		{
+			printf("ERROR LINKS\n");
+			return (0);
+		}
+		tetri = get_tetris(buffer);
+		print_tetri(tetri);
+		ft_lstpushback(&list, ft_lstnew(tetri, 32));
 	}
-	// print_list(list);
 	return (1);
 }
